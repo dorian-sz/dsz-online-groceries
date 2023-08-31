6561072eb567e2779dea22b85df0a2cc68dc8f2c
Dorian <dorian.szatmari@gmail.com>
2023-08-31 08:42:35 +0200

    Add navbar

--- dsz-online-groceries-app/app/(root)/layout.tsx
+++ dsz-online-groceries-app/app/(root)/layout.tsx
@@ -1,7 +1,18 @@
+import Navbar from "@/components/navbar";
+
 export default function MainLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <div>{children}</div>;
+  return (
+    <div className="h-full relative bg-zinc-100">
+      <nav className="flex flex-col fixed w-full h-24 md:h-48 bg-white">
+        <Navbar />
+      </nav>
+      <main className="pt-24 px-2 w-full md:w-3/5 md:mx-auto md:pt-48 md:px-0">
+        {children}
+      </main>
+    </div>
+  );
 }
