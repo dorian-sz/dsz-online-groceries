9420da7d81a173802cc3021cd9c19663e08f391c
Dorian <dorian.szatmari@gmail.com>
2023-09-01 02:14:57 +0200

    Add  logo to navbar

--- dsz-online-goceries-admin/components/navbar.tsx
+++ dsz-online-goceries-admin/components/navbar.tsx
@@ -1,12 +1,18 @@
 import { Button } from "@/components/ui/button";
 import { SignOutButton } from "@clerk/nextjs";
 import NavRoutes from "./nav-routes";
+import Logo from "./ui/logo";
 
 const Navbar = () => {
   return (
     <nav className="h-20 p-4 w-full bg-white">
       <div className="flex items-center mx-auto w-1/2">
-        <NavRoutes />
+        <div className="flex w-1/2 justify-between items-center">
+          <div className="flex items-center">
+            <Logo width={140} height={70} />
+          </div>
+          <NavRoutes />
+        </div>
         <div className="ml-auto">
           <SignOutButton>
             <Button>Logout</Button>
