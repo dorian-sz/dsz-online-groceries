3fb99f83988f7746f265a0a3206ff3bb3f3204c8
Dorian <dorian.szatmari@gmail.com>
2023-09-01 02:15:24 +0200

    Add logo component

---
+++ dsz-online-goceries-admin/components/ui/logo.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import Image from "next/image";
+
+interface LogoProps {
+  width: number;
+  height: number;
+}
+
+const Logo: React.FC<LogoProps> = ({ width, height }) => {
+  return (
+    <Image
+      alt="logo"
+      className="cursor-pointer"
+      height={height}
+      width={width}
+      src="/images/logo.png"
+    />
+  );
+};
+
+export default Logo;
