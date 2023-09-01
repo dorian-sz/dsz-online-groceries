2f7694a89450bb95180ca81c8c7476ac31fae0ce
Dorian <dorian.szatmari@gmail.com>
2023-09-01 02:14:03 +0200

    Add category apis

---
+++ dsz-online-goceries-admin/app/api/categories/route.ts
@@ -0,0 +1,41 @@
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+    const { name } = body;
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+    if (!name) {
+      return new NextResponse("Name is required", { status: 400 });
+    }
+
+    const category = await prismadb.category.create({
+      data: {
+        name,
+      },
+    });
+
+    return NextResponse.json(category);
+  } catch (error) {
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
+
+export async function GET(
+  req: Request,
+  { params }: { params: { categoryId: string } }
+) {
+  try {
+    const category = await prismadb.category.findMany();
+
+    return NextResponse.json(category);
+  } catch (error) {
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
