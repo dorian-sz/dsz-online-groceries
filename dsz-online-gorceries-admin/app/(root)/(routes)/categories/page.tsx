ca102f69fa0dd1f19c5f652149d182d701fddf49
Dorian <dorian.szatmari@gmail.com>
2023-09-08 11:56:54 +0200

    Add categories page

--- dsz-online-gorceries-admin/app/(root)/(routes)/categories/page.tsx
+++ dsz-online-gorceries-admin/app/(root)/(routes)/categories/page.tsx
@@ -1,5 +1,18 @@
+import Link from "next/link";
+
+import Heading from "@/components/heading";
+import { Button } from "@/components/ui/button";
+
 const CategoriesPage = () => {
-  return <div>Categories Page!</div>;
+  return (
+    <div className="flex flex-col gap-y-8">
+      <Heading
+        label="Add Category"
+        title="Manage Categories"
+        href="/categories/new"
+      />
+    </div>
+  );
 };
 
 export default CategoriesPage;
