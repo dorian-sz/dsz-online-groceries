9c81e6b553fb541b4cf893f170051564fd72befe
Dorian <dorian.szatmari@gmail.com>
2023-09-09 12:50:24 +0200

    Add patch method for updating category

--- dsz-online-gorceries-admin/app/api/categories/[categoryId]/route.ts
+++ dsz-online-gorceries-admin/app/api/categories/[categoryId]/route.ts
@@ -21,3 +21,29 @@ export async function DELETE(
     return new NextResponse("Error", { status: 500 });
   }
 }
+
+export async function PATCH(
+  request: Request,
+  { params }: { params: { categoryId: string } }
+) {
+  try {
+    const { userId } = auth();
+    const body = await request.json();
+    const { name } = body;
+
+    if (!userId) return new NextResponse("Unauthorized", { status: 401 });
+    if (!name) new NextResponse("Category name required!", { status: 400 });
+
+    const category = await prismadb.category.update({
+      where: {
+        id: params.categoryId,
+      },
+      data: {
+        name: name,
+      },
+    });
+    return NextResponse.json(category);
+  } catch (error) {
+    return new NextResponse("Error", { status: 500 });
+  }
+}
