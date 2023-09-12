94b1626950951640026aff05b00b34c185b1329d
Dorian <dorian.szatmari@gmail.com>
2023-09-12 21:06:57 +0200

    Add products page

--- dsz-online-gorceries-admin/app/(root)/(routes)/products/page.tsx
+++ dsz-online-gorceries-admin/app/(root)/(routes)/products/page.tsx
@@ -1,5 +1,21 @@
-const ProductsPage = () => {
-  return <div>Products Page!</div>;
+import Heading from "@/components/heading";
+import { DataTable } from "@/components/ui/data-table";
+import { Columns } from "./components/columns";
+import prismadb from "@/lib/prismadb";
+
+const ProductsPage = async () => {
+  const data = await prismadb.product.findMany({ include: { category: true } });
+  console.log(data);
+  return (
+    <div className="flex flex-col gap-y-8">
+      <Heading
+        label="Add Product"
+        title="Manage Productss"
+        href="/products/new"
+      />
+      <DataTable searchKey="name" columns={Columns} data={data} />
+    </div>
+  );
 };
 
 export default ProductsPage;
