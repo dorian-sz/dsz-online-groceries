acb5ae9e99abdbdc9305168b408525c043ed5702
Dorian <dorian.szatmari@gmail.com>
2023-09-13 16:01:55 +0200

    Add post route for product

--- dsz-online-gorceries-admin/app/api/products/route.ts
+++ dsz-online-gorceries-admin/app/api/products/route.ts
@@ -17,3 +17,27 @@ export async function GET() {
     return new NextResponse(`Error: ${error}`, { status: 500 });
   }
 }
+
+export async function POST(request: Request) {
+  try {
+    const { userId } = auth();
+    const body = await request.json();
+    const { name, price, image, categoryId } = body;
+
+    if (!userId) new NextResponse("Unauthorized", { status: 401 });
+    if (!name || !price || !image || categoryId)
+      new NextResponse("All product data is required!", { status: 400 });
+    const product = await prismadb.product.create({
+      data: {
+        name,
+        price,
+        image,
+        categoryId,
+      },
+    });
+    console.log(product);
+    return NextResponse.json(product);
+  } catch (error) {
+    return new NextResponse(`Error: ${error}`, { status: 500 });
+  }
+}
