e1bc9e59b356b20732752e41890970b7d018d88c
Dorian <dorian.szatmari@gmail.com>
2023-09-13 23:12:41 +0200

    Add formatted initial data

--- dsz-online-gorceries-admin/app/(root)/(routes)/products/page.tsx
+++ dsz-online-gorceries-admin/app/(root)/(routes)/products/page.tsx
@@ -4,13 +4,20 @@ import { Columns } from "./components/columns";
 import prismadb from "@/lib/prismadb";
 
 const ProductsPage = async () => {
-  const data = await prismadb.product.findMany({ include: { category: true } });
-  console.log(data);
+  const products = await prismadb.product.findMany({
+    include: { category: true },
+  });
+  const data = products.map((product) => ({
+    ...product,
+    price: product.price.toString(),
+    category: product.category.name,
+  }));
+
   return (
     <div className="flex flex-col gap-y-8">
       <Heading
         label="Add Product"
-        title="Manage Productss"
+        title="Manage Products"
         href="/products/new"
       />
       <DataTable searchKey="name" columns={Columns} data={data} />
