cacbbd5a79edb3bd8f048f7703a0cfb2914d8085
Dorian <dorian.szatmari@gmail.com>
2023-09-13 23:14:48 +0200

    Add patch route for product

--- dsz-online-gorceries-admin/app/api/products/[productId]/route.ts
+++ dsz-online-gorceries-admin/app/api/products/[productId]/route.ts
@@ -22,3 +22,30 @@ export async function DELETE(
     return new NextResponse(`Error: ${error}`, { status: 500 });
   }
 }
+
+export async function PATCH(
+  request: Request,
+  { params }: { params: { productId: string } }
+) {
+  try {
+    const { userId } = auth();
+    const body = await request.json();
+    const { name, price, image, categoryId } = body;
+    console.log(body);
+    if (!name || !price || !image || categoryId)
+      new NextResponse("All product data is required!", { status: 400 });
+    const product = await prismadb.product.update({
+      where: { id: params.productId },
+      data: {
+        name,
+        price,
+        image,
+        categoryId,
+      },
+    });
+
+    return NextResponse.json(product);
+  } catch (error) {
+    return new NextResponse(`Error: ${error}`, { status: 500 });
+  }
+}
