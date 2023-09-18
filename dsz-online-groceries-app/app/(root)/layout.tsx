4adcdcbd725eb5d3deffea903f53c8748913219e
Dorian <dorian.szatmari@gmail.com>
2023-09-18 21:40:08 +0200

    Change main container size

--- dsz-online-groceries-app/app/(root)/layout.tsx
+++ dsz-online-groceries-app/app/(root)/layout.tsx
@@ -10,7 +10,7 @@ export default function MainLayout({
       <nav className="flex flex-col fixed w-full h-24 md:h-56 bg-white">
         <Navbar />
       </nav>
-      <main className="pt-24 px-4 w-full md:w-3/5 md:mx-auto md:pt-56 md:px-2">
+      <main className="pt-24 w-full md:w-3/4 md:mx-auto md:pt-64">
         {children}
       </main>
     </div>
