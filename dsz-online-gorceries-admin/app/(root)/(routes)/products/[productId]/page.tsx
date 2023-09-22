2438ec6576b71ec6feaf50b31e318d2ba83081c0
Dorian <dorian.szatmari@gmail.com>
2023-09-22 19:55:26 +0200

    Remove subcategories

--- dsz-online-gorceries-admin/app/(root)/(routes)/products/[productId]/page.tsx
+++ dsz-online-gorceries-admin/app/(root)/(routes)/products/[productId]/page.tsx
@@ -7,12 +7,12 @@ const ProductPage = async ({ params }: { params: { productId: string } }) => {
       id: params.productId,
     },
     include: {
-      category: true,
+      categories: true,
+      offers: true,
     },
   });
 
   const categories = await prismadb.category.findMany();
-  const subcategories = await prismadb.subcategory.findMany();
   const offers = await prismadb.offer.findMany();
 
   return (
@@ -20,7 +20,6 @@ const ProductPage = async ({ params }: { params: { productId: string } }) => {
       <ProductForm
         initialData={product}
         categories={categories}
-        subcategories={subcategories}
         offers={offers}
       />
     </div>
