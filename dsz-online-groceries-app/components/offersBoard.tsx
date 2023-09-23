75c3bec9a89e5327cb0274fec20bcfe6769e862a
Dorian <dorian.szatmari@gmail.com>
2023-09-23 20:23:46 +0200

    Add offer cards to offer board

--- dsz-online-groceries-app/components/offersBoard.tsx
+++ dsz-online-groceries-app/components/offersBoard.tsx
@@ -1,19 +1,25 @@
-"use client"
+"use client";
 
+import { Offer } from "@/data/types";
 import { usePathname } from "next/navigation";
 import { useMemo } from "react";
+import OfferCard from "@/components/ui/offerCard";
 
-const OffersBoard = () => {
-    const pathname = usePathname()
-    const offers = useMemo(()=>[
-        {
-            label: "£1 & under"
-        }
-    ],[pathname])
+interface OffersBoardProps {
+  offers: Offer[];
+}
+
+const OffersBoard: React.FC<OffersBoardProps> = ({ offers }) => {
+  const pathname = usePathname();
 
   return (
-    <div className="flex flex-col w-full items-center p-4 justify-center bg-white">
+    <div className="flex flex-col w-full items-center p-6 justify-center gap-y-8 bg-white">
       <p className="text-4xl font-extrabold">Offers</p>
+      <div className="flex gap-4 flex-wrap justify-center items-center mx-auto w-3/4">
+        {offers.map((offer) => (
+          <OfferCard name={offer.name} />
+        ))}
+      </div>
     </div>
   );
 };
