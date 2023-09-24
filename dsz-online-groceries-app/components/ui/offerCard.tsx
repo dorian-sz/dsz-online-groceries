9e2231fc31f758211beff049c926e90e612f4643
Dorian <dorian.szatmari@gmail.com>
2023-09-24 23:28:54 +0200

    Add changed background when offer card active

--- dsz-online-groceries-app/components/ui/offerCard.tsx
+++ dsz-online-groceries-app/components/ui/offerCard.tsx
@@ -1,13 +1,20 @@
+import { cn } from "@/lib/utils";
 import Link from "next/link";
 
 interface OfferCardProps {
   id: string;
   name: string;
+  active: boolean;
 }
 
-const OfferCard: React.FC<OfferCardProps> = ({ id, name }) => {
+const OfferCard: React.FC<OfferCardProps> = ({ id, name, active }) => {
   return (
-    <div className="flex rounded-3xl font-bold bg-zinc-100 text-md py-2 px-4 cursor-pointer hover:bg-zinc-200">
+    <div
+      className={cn(
+        "flex-shrink-0 w-fit rounded-3xl font-bold  text-md py-2 px-4 cursor-pointer ",
+        active ? "bg-pink-900 text-white" : "bg-zinc-100 hover:bg-zinc-200"
+      )}
+    >
       <Link href={`/offers/${id}`}>
         <p>{name}</p>
       </Link>
