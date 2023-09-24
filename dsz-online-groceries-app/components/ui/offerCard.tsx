0c593140dd49d044198ad4fe3d8e7080238767f3
Dorian <dorian.szatmari@gmail.com>
2023-09-24 10:16:29 +0200

    Add Link to offer card

--- dsz-online-groceries-app/components/ui/offerCard.tsx
+++ dsz-online-groceries-app/components/ui/offerCard.tsx
@@ -1,11 +1,16 @@
+import Link from "next/link";
+
 interface OfferCardProps {
+  id: string;
   name: string;
 }
 
-const OfferCard: React.FC<OfferCardProps> = ({ name }) => {
+const OfferCard: React.FC<OfferCardProps> = ({ id, name }) => {
   return (
     <div className="flex rounded-3xl font-bold bg-zinc-100 text-md py-2 px-4 cursor-pointer hover:bg-zinc-200">
-      <p>{name}</p>
+      <Link href={`/offers/${id}`}>
+        <p>{name}</p>
+      </Link>
     </div>
   );
 };
