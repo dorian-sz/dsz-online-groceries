9a812bb0cecf9abde8f9f3734f7cd002cd9dee54
Dorian <dorian.szatmari@gmail.com>
2023-09-26 17:54:29 +0200

    Add get product function

--- dsz-online-groceries-app/data/products.ts
+++ dsz-online-groceries-app/data/products.ts
@@ -10,3 +10,10 @@ export const getProducts = async (query: {
   );
   return data.data;
 };
+
+export const getProduct = async (id: string): Promise<Product> => {
+  const data = await axios.get<Product>(
+    `http://localhost:3000/api/products/${id}`
+  );
+  return data.data;
+};
