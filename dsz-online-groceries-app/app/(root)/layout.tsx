fb68cd00ab23cac17cdb1fe8765d0f3c6e4abd91
Dorian <dorian.szatmari@gmail.com>
2023-09-26 17:51:28 +0200

    Change layout top padding

--- dsz-online-groceries-app/app/(root)/layout.tsx
+++ dsz-online-groceries-app/app/(root)/layout.tsx
@@ -10,7 +10,7 @@ export default function MainLayout({
       <nav className="flex flex-col fixed w-full h-24 md:h-56 bg-white">
         <Navbar />
       </nav>
-      <main className="pt-24 w-full md:w-3/4 md:mx-auto md:pt-64">
+      <main className="pt-28 w-full md:w-3/4 md:mx-auto md:pt-64">
         {children}
       </main>
     </div>
