90054ea53ee2bb4ae6a1de6aaf13385e0c7a57af
Dorian <dorian.szatmari@gmail.com>
2023-09-27 13:07:21 +0200

    Add units data

---
+++ dsz-online-gorceries-admin/prisma/data.js
@@ -0,0 +1,27 @@
+const units = [
+  {
+    name: "kg",
+  },
+  {
+    name: "g",
+  },
+  {
+    name: "ltr",
+  },
+  {
+    name: "cl",
+  },
+  {
+    name: "ml",
+  },
+  {
+    name: "sht",
+  },
+  {
+    name: "wipe",
+  },
+];
+
+module.exports = {
+  units,
+};
