e1e73894649aaf0839340cd0ee144b718ccb0f9f
Dorian <dorian.szatmari@gmail.com>
2023-09-28 11:36:21 +0200

    Add price formatting to product card

--- dsz-online-groceries-app/components/productCard.tsx
+++ dsz-online-groceries-app/components/productCard.tsx
@@ -1,43 +1,69 @@
 "use client";
 
-import { Product } from "@/data/types";
+import { Product, Unit } from "@/data/types";
 import { CldImage } from "next-cloudinary";
 import { Button } from "./ui/button";
 import Link from "next/link";
+import { displayPrice, unitPrice } from "@/app/helpers/priceFomat";
 
 interface ProductCardProps {
   product: Product;
 }
 
 const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
+  const strMaxLength = 60;
+  const uPrice = unitPrice(product.price, product.size, product.unit.factor);
+  const truncate = (str: string, maxLength: number) => {
+    return str.length > maxLength ? str.substring(0, maxLength) + "..." : str;
+  };
+
   return (
-    <div className="flex flex-col gap-y-8 p-4 pt-20 w-full md:w-[18.65%] md:h-full justify-center items-center bg-white">
-      <Link href={`/product/${product.id}`}>
-        <div className="flex md:flex-col">
-          <div className="flex items-center justify-center">
-            <CldImage
-              className="rounded-sm"
-              width={140}
-              height={140}
-              alt="uploadImagePreview"
-              src={product.image}
-            />
-          </div>
-          <div className="flex flex-col gap-y-8 text-sm w-full h-full justify-between font-semibold">
-            <div>
-              <p>{product.name}</p>
-            </div>
-            <div>
-              <p className="text-lg font-bold">
-                £{parseFloat(product.price.toString())}
-              </p>
-            </div>
-          </div>
+    <div className="flex flex-row md:flex-col gap-y-8 p-4 w-full md:w-[18.7%] md:h-full justify-end items-center bg-white">
+      <div className="hidden md:flex">
+        <CldImage
+          className="rounded-sm"
+          width={140}
+          height={140}
+          alt="uploadImagePreview"
+          src={product.image}
+        />
+      </div>
+      <div className="flex w-1/2 md:hidden">
+        <CldImage
+          className="rounded-sm"
+          width={100}
+          height={100}
+          alt="uploadImagePreview"
+          src={product.image}
+        />
+      </div>
+      <div className="flex flex-col w-full gap-y-4">
+        <div>
+          <Link href={`/product/${product.id}`}>
+            <p className="font-semibold text-sm transition-colors duration-200 hover:underline hover:text-orange-600">
+              {truncate(product.name, strMaxLength)}
+            </p>
+          </Link>
         </div>
-        <div className="flex w-full justify-end md:justify-center items-center">
-          <Button className="w-1/2 py-4 md:w-3/4 font-bold">Add</Button>
+        <div className="flex flex-col gap-y-1">
+          <p className="text-sm">
+            <span className="text-purple-700 font-extrabold text-base">
+              {displayPrice(parseFloat(product.nectarPrice.toString()))}
+            </span>{" "}
+            with Nectar
+          </p>
+          <p className="text-sm">
+            <span className="font-extrabold text-xl">
+              {displayPrice(parseFloat(product.price.toString()))}
+            </span>{" "}
+            {displayPrice(parseFloat(uPrice.toString()))} /{" "}
+            {product.unit ? product.unit.name : "ea"}
+          </p>
         </div>
-      </Link>
+        <div className="flex justify-end md:items-center md:justify-center w-full">
+          <Button className="w-4/6 ">Add</Button>
+        </div>
+      </div>
     </div>
   );
 };
