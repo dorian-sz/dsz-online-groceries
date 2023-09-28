a2455c6e4fb158ac953b008e28016c5d145622ad
Dorian <dorian.szatmari@gmail.com>
2023-09-28 16:17:52 +0200

    Add nectar tag

---
+++ dsz-online-groceries-app/components/nectar.tsx
@@ -0,0 +1,9 @@
+const Nectar = () => {
+  return (
+    <div className="flex items-center bg-purple-600 w-fit text-sm font-bold text-white p-1">
+      <p>Nectar Price</p>
+    </div>
+  );
+};
+
+export default Nectar;
