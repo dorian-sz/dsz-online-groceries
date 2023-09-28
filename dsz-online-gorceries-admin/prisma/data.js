62011953ac9803c77802dee87d160ef7f45d8ac6
Dorian <dorian.szatmari@gmail.com>
2023-09-28 11:37:16 +0200

    Add seeding values

--- dsz-online-gorceries-admin/prisma/data.js
+++ dsz-online-gorceries-admin/prisma/data.js
@@ -1,27 +1,67 @@
 const units = [
   {
     name: "kg",
+    factor: 1,
   },
   {
-    name: "g",
+    name: "100g",
+    factor: 100,
   },
   {
     name: "ltr",
+    factor: 1,
   },
   {
     name: "cl",
+    factor: 10,
   },
   {
-    name: "ml",
+    name: "100ml",
+    factor: 100,
   },
   {
-    name: "sht",
+    name: "100sht",
+    factor: 1,
+  },
+];
+
+const offers = [
+  {
+    name: "£1 & under",
   },
   {
-    name: "wipe",
+    name: "£2 & under",
+  },
+  {
+    name: "Beers, wines & spirits",
+  },
+  {
+    name: "Chilled & frozen",
+  },
+  {
+    name: "Food Cupboard",
+  },
+  {
+    name: "Fruit & veg",
+  },
+  {
+    name: "Household",
+  },
+  {
+    name: "Meat & fish",
+  },
+  {
+    name: "Nectar prices",
+  },
+  {
+    name: "Snacks & treats",
+  },
+  {
+    name: "Toiletries, health & beauty",
   },
 ];
 
 module.exports = {
   units,
+  offers,
 };
