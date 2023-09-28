2155110669ece776ea46210534c7a519c07028fe
Dorian <dorian.szatmari@gmail.com>
2023-09-28 16:17:45 +0200

    Add z index to nav container

--- dsz-online-groceries-app/app/(root)/layout.tsx
+++ dsz-online-groceries-app/app/(root)/layout.tsx
@@ -7,7 +7,7 @@ export default function MainLayout({
 }) {
   return (
     <div className=" h-screen overflow-auto relative bg-zinc-100">
-      <nav className="flex flex-col fixed w-full h-24 md:h-56 bg-white">
+      <nav className="flex flex-col z-50 fixed w-full h-24 md:h-56 bg-white">
         <Navbar />
       </nav>
       <main className="pt-28 w-full md:w-3/4 md:mx-auto md:pt-64">
