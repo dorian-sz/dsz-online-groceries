229bb04f285fa15555298e78eb099fddb3c4efd3
Dorian <dorian.szatmari@gmail.com>
2023-10-03 10:25:22 +0200

    Add on mouse events for displaying cart

--- dsz-online-groceries-app/components/cart.tsx
+++ dsz-online-groceries-app/components/cart.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@/components/ui/button";
 import { ShoppingCart } from "lucide-react";
 import { useEffect, useState } from "react";
+import CartHover from "./cartHover";
 
 const Cart = () => {
   const [isMounted, setIsMounted] = useState(false);
+  const [open, setOpen] = useState(false);
 
   useEffect(() => {
     setIsMounted(true);
@@ -14,10 +16,25 @@ const Cart = () => {
   }
 
   return (
-    <Button className="gap-x-2 px-4">
-      <ShoppingCart />
-      <span className="font-bold text-sm">£0.00</span>
-    </Button>
+    <div className="relative">
+      <Button
+        className="gap-x-2 px-4"
+        onMouseEnter={() => setOpen(true)}
+        onMouseLeave={() => setOpen(false)}
+      >
+        <ShoppingCart />
+        <span className="font-bold text-sm">£0.00</span>
+      </Button>
+      {open && (
+        <div
+          className="flex items-end absolute z-50 top-10 right-0 w-[440px] h-[700px]"
+          onMouseEnter={() => setOpen(true)}
+          onMouseLeave={() => setOpen(false)}
+        >
+          <CartHover />
+        </div>
+      )}
+    </div>
   );
 };
 
