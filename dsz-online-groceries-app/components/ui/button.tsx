610d7458b9f20a6b95b23de401353f5861331ada
Dorian <dorian.szatmari@gmail.com>
2023-10-03 10:26:06 +0200

    Add orange colour to ghost and outline variant

--- dsz-online-groceries-app/components/ui/button.tsx
+++ dsz-online-groceries-app/components/ui/button.tsx
@@ -14,10 +14,10 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+          "border-2 border-orange-600 text-orange-600 hover:bg-orange-500/10",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
+        ghost: "hover:bg-orange-500/10",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
