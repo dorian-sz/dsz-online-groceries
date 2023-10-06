c93270ac2b65353cb337667ac28195519b06ef08
Dorian <dorian.szatmari@gmail.com>
2023-10-06 10:46:06 +0200

    Add quantity to cart items

--- dsz-online-groceries-app/redux/slices/cart-slice.ts
+++ dsz-online-groceries-app/redux/slices/cart-slice.ts
@@ -3,7 +3,7 @@ import { Product } from "@/data/types";
 
 const initialState = {
   loading: true,
-  cartItems: <Product[]>[],
+  cartItems: [] as { product: Product; quantity: Number }[],
   totalPrice: 0,
 };
 
@@ -12,17 +12,27 @@ export const cartSlice = createSlice({
   initialState,
   reducers: {
     addToCart: (state, action) => {
-      const item: Product = action.payload;
-      const exists = state.cartItems.find((product) => product.id === item.id);
+      const item = action.payload;
+      const product: Product = item.product;
+      const exists = state.cartItems.find(
+        (cartItem) => cartItem.product.id === item.id
+      );
       if (exists) {
-        state.cartItems = state.cartItems.map((product) =>
-          product.id === exists.id ? item : product
+        state.cartItems = state.cartItems.map((cartItem) =>
+          cartItem.product.id === exists.product.id ? item : cartItem
         );
       } else {
         state.cartItems = [...state.cartItems, item];
       }
+      item.quantity++;
       state.totalPrice = parseFloat(
-        (Number(state.totalPrice) + Number(item.price)).toFixed(2)
+        state.cartItems
+          .reduce(
+            (acc, cartItem) =>
+              acc + Number(cartItem.product.price) * Number(cartItem.quantity),
+            0
+          )
+          .toFixed(2)
       );
     },
     removeFromCart: (state, action) => {},
