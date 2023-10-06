3c4ef01a1618ac8b05daf65b0570dd19a52134ce
Dorian <dorian.szatmari@gmail.com>
2023-10-06 10:23:52 +0200

    Add store provider to layout

--- dsz-online-groceries-app/app/layout.tsx
+++ dsz-online-groceries-app/app/layout.tsx
@@ -1,3 +1,4 @@
+import { StoreProvider } from "@/redux/StoreProvider";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
@@ -16,7 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <StoreProvider>
+        <body className={inter.className}>{children}</body>
+      </StoreProvider>
     </html>
   );
 }
